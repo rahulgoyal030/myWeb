bbc9154479141f5ab3c961980e290f2b0269ad15
rahulgoyal030 <goyal.rahul030@gmail.com>
2015-06-17 01:19:21 +0530

    3rd commit

---
+++ index.js
@@ -0,0 +1,20 @@
+$(document).ready(function  () {
+
+	$("#signUp").click(function  () {
+        
+        $("body").fadeOut(1000,function() {
+        	window.open("signUp.php","_parent","width=500, height=300,left=100,top=50");
+        });
+		
+		// body...
+	});
+	
+});
+
+// $(document).ready(function  () {
+//  $("#signUp").before("<p>new here signUp here ! </p>");
+//  	// body...
+
+// });
+
+
